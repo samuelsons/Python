2b1203b3fe2de07aae363bab633ab957ed8883c8
Samuel Osoba <samuelsontech@gmail.com>
2023-10-05 11:45:36 +0100

    Commandline Programs

--- basic-level.py
+++ basic-level.py
@@ -21,7 +21,20 @@ import sys
 # print("Greetings " + name)
 
 # Working with numbers
-print("calculator progrram")
-first_number = input("First number: ")
-second_number = input("Second number: ")
-print("The result of the calculated number is: ", int(first_number) + int(second_number))
+# print("calculator progrram")
+# first_number = input("First number: ")
+# second_number = input("Second number: ")
+# print("The result of the calculated number is: ", int(first_number) % int(second_number))
+
+# Program: Convert parsecs to lights and display the results.
+# parsecs = 11
+# lightyears = parsecs * 3.26
+# print(str(parsecs) + " Parsecs is equal to " + str(lightyears) + " lightyears.")
+
+
+# Re-factor code to allow program user input no. of parsecs for conversion.
+print("::-- Parsecs to Light years converted pogram.")
+parsecs_input = input("Enter the number of parsecs: ")
+parsecs = int(parsecs_input)
+lightyears = parsecs * 3.26
+print(str(parsecs) + " parsecs expressed in lightyears equals " + str(lightyears) + " lightyears")
